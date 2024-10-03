e7f8da45e2b4ea560b818e342e1a9196eea3e121
samebdon <33068203+samebdon@users.noreply.github.com>
2024-10-03 17:41:59 +0100

    remaps allele naming based on allele frequency

--- vcf2diem.py
+++ vcf2diem.py
@@ -7,8 +7,8 @@ Usage:
 
 Options:
  -v, --vcf <FILE>                       VCF file
- -l, --location                         Print SNP location
- -n, --non_callable_limit <INT>         Maximum number of noncallable genotypes per site (default = no limit)
+ -n, --no_location                      Turn off SNP location printing
+ -l, --non_callable_limit <INT>         Maximum number of noncallable genotypes per site (default = no limit)
  -h, --help                             Print this message
 """
 
@@ -24,6 +24,83 @@ from tqdm import tqdm
 from timeit import default_timer as timer
 from docopt import docopt
 
+class GenotypeData:
+    def __init__(
+        self,
+        chromosome,
+        vcf_dict,
+    ):
+        self.chromosome = chromosome
+        self.vcf_dict = vcf_dict
+        self.mask_array = None
+        self.genotype_array = None
+        self.most_common_alleles = None
+        self.second_most_common_alleles = None
+        self.positions = None
+
+    def get_mask_array(self, biallelic=False):
+        is_chrom_array = self.vcf_dict["variants/CHROM"] == self.chromosome
+        is_SNP_array = self.vcf_dict["variants/is_snp"]
+
+        mask_array = (is_SNP_array == True) & (is_chrom_array == True)
+        if isinstance(self.vcf_dict["variants/NUMALT"][0], int) & biallelic:
+            numalt_array = self.vcf_dict["variants/NUMALT"]
+            mask_array = mask_array & (numalt_array == 1) # here i am taking sites with only 1 alt allele
+            
+        self.mask_array = mask_array
+
+    def get_genotype_array(self):
+        """
+        filter out sites which are singletons, invariant, or lack homozygotes here
+        my non singletons function might be better and invariant might not matter, theres only 5 invariant sites
+        any way to count the different genotypes specifically? maybe in a dictionary?
+
+        self.mask_array = np.logical_and(self.mask_array, mask_array)
+        """
+        snp_gts = self.vcf_dict["calldata/GT"][self.mask_array]
+        self.genotype_array = allel.GenotypeArray(snp_gts)
+        print(self.genotype_array.is_hom())
+        sys.exit()
+
+
+    def get_allele_order(self):
+        """
+        Currently doesnt account for ties just uses what numpy picks
+        Tossing a coin would be preferable
+        """
+        acs = self.genotype_array.count_alleles()
+        self.acs = np.flip(acs.argsort(), axis=1) 
+
+
+    def map_alleles(self):
+        mapping = self.acs
+        if mapping.shape[1] > 2:
+            mapping[:,2:] = -2
+        self.genotype_array = self.genotype_array.map_alleles(mapping)
+
+    def get_pos(self):
+        self.positions =  self.vcf_dict["variants/POS"][self.mask_array]
+    
+    def convert_to_diem_df(self, limit=None):
+        """
+        Assumes the genotype array is coded so that 0 is the most frequent allele
+        and 1 is the second most frequent allele. 
+
+        All other alleles are excluded!
+        """
+        snp_ga = self.genotype_array
+        snp_ga[snp_ga > 1] = -2
+        snp_ga[snp_ga < 0] = -2
+        summed_snp_ga = np.sum(snp_ga, axis=2)
+
+        df = pd.DataFrame(summed_snp_ga)
+        df.replace([i for i in range(-1,-10,-1)], "_", inplace=True)
+        S = pd.DataFrame(["S"] * df.shape[0], columns=['S'])
+        pos = pd.DataFrame(self.positions, columns=['pos'])
+        df = pd.concat([pos, S, df], axis=1)
+        non_singletons = get_non_singletons_list(df, limit)
+        return df.loc[non_singletons, :]
+
 def load_vcf(vcf_f):
     query_fields = [
         "samples",
@@ -38,63 +115,25 @@ def load_vcf(vcf_f):
 
     return vcf_dict
 
-
-def get_chromosome_data(vcf_dict, chromosome_name):
-
-    is_chrom_array = vcf_dict["variants/CHROM"] == chromosome_name
-
-    is_SNP_array = vcf_dict["variants/is_snp"]
-
-    if isinstance(vcf_dict["variants/NUMALT"][0], int):
-        numalt_array = vcf_dict["variants/NUMALT"]
-        mask_array = (
-            (numalt_array == 1) & (is_SNP_array == True) & (is_chrom_array == True)
-        )
-    else:
-        mask_array = (is_SNP_array == True) & (is_chrom_array == True)
-
-    snp_pos = vcf_dict["variants/POS"][mask_array]
-    snp_gts = vcf_dict["calldata/GT"][mask_array]
-    snp_ga = allel.GenotypeArray(snp_gts)
-    snp_ga[snp_ga > 1] = -2
-    snp_ga[snp_ga < 0] = -2
-    summed_snp_ga = np.sum(snp_ga, axis=2)
-    df = pd.DataFrame(summed_snp_ga)
-    df = df.replace([i for i in range(-1,-10,-1)], "_")
-    S = pd.DataFrame(["S"] * df.shape[0])
-    pos = pd.DataFrame(snp_pos)
-    df = pd.concat([pos, S, df], axis=1)
-    colnames = list(df.columns)
-    colnames[0]='pos'
-    colnames[1]='S'
-    df.columns=colnames
-    non_singletons_list = non_singletons(df)
-    df['non_singletons'] = non_singletons_list
-    df = df.loc[df.non_singletons, :]
-    return df
-
-
-def write_diem(df, chromosome_name):
-    df.drop(columns=['non_singletons'], inplace=True)
+def write_diem(df, chromosome_name, print_pos=True):
     if print_pos:
         np.savetxt(
-            "./diem_files/snp_pos/" + str(chromosome_name) + ".snp_pos.diem.txt",
+            f"./diem_files/snp_pos/{str(chromosome_name)}.snp_pos.diem.txt",
             df['pos'].values,
             fmt="%s",
             delimiter="",
         )
-
     
     df.drop(columns=['pos'], inplace=True)
     np.savetxt(
-        "./diem_files/diem_input/" + str(chromosome_name) + ".diem.txt",
+        f"./diem_files/diem_input/{str(chromosome_name)}.diem.txt",
         df.values,
         fmt="%s",
         delimiter="",
     )
-    print("Chromosome: " + str(chromosome_name) + "...")
+    print(f"Chromosome: {str(chromosome_name)} written")
 
-def non_singletons(df):
+def get_non_singletons_list(df, limit=None):
     new_df = df.drop(columns=['pos'])
     new_df.drop(columns=['S'], inplace=True)
     unc_count_arr = (new_df.to_numpy() == '_').sum(axis=1)
@@ -103,8 +142,8 @@ def non_singletons(df):
     max_sum_arr = [2*(new_df.shape[1]-unc_count) for unc_count in unc_count_arr]
     non_singletons = [False if (x<=1) or (x>=max_sum-1) else True for x, max_sum in zip(row_sum_arr, max_sum_arr)]
 
-    if args["--non_callable_limit"]:
-        limit = int(args["--non_callable_limit"])
+    if limit:
+        limit = int(limit)
         unc_bool_arr = unc_count_arr <= limit
         non_singletons_pass = np.logical_and(non_singletons, unc_bool_arr)
         return non_singletons_pass
@@ -112,16 +151,11 @@ def non_singletons(df):
     else:
         return non_singletons
 
-if __name__ == "__main__":
 
+def main():
     args = docopt(__doc__)
 
     vcf_f = args["--vcf"]
-
-    if args["--location"]:
-        print_pos = True
-    else:
-        print_pos = False
 
     try:
         start_time = timer()
@@ -129,25 +163,35 @@ if __name__ == "__main__":
         path = os.path.join("./diem_files/diem_input/")
         os.makedirs(path, exist_ok=True)
 
-        if print_pos:
+        if args["--no_location"]:
+            print_pos = False
+        else:
+            print_pos = True
             path = os.path.join("./diem_files/snp_pos/")
             os.makedirs(path, exist_ok=True)
 
         vcf_dict = load_vcf(vcf_f=vcf_f)
+        print(f'Loaded VCF file: {vcf_f}')
 
         all_contigs = np.unique(vcf_dict["variants/CHROM"])
-        chromosome_names = np.array([x for x in all_contigs if 'contig' not in x])
+        chromosome_names = [x for x in all_contigs if 'CAM' not in x] #need better contig filter
 
         for chromosome in chromosome_names:
-            chromosome_data = get_chromosome_data(
-                vcf_dict=vcf_dict, chromosome_name=chromosome
-            )
-            write_diem(df=chromosome_data, chromosome_name=chromosome)
 
-        print("[+] Total runtime: %.3fs" % (timer() - start_time))
+            chromosome_genotype_data = GenotypeData(chromosome, vcf_dict)
+            chromosome_genotype_data.get_mask_array()
+            chromosome_genotype_data.get_genotype_array()
+            chromosome_genotype_data.get_allele_order()
+            chromosome_genotype_data.map_alleles() #sets most common and second most common alleles to 0 and 1, and everything else to -2
+            chromosome_genotype_data.get_pos()
+            diem_df = chromosome_genotype_data.convert_to_diem_df(args['--non_callable_limit'])
+            write_diem(diem_df, chromosome, print_pos)
 
     except KeyboardInterrupt:
         sys.stderr.write(
             "\n[X] Interrupted by user after %i seconds!\n" % (timer() - start_time)
         )
         sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
